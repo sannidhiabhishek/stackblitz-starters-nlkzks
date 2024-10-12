c48224be503d1001a18db06969f291f6cf51d7ae
sannidhiabhishek <38261599+sannidhiabhishek@users.noreply.github.com>
2024-10-12 15:10:26 +0530

    Math js update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { evaluate } from 'mathjs'
+import { evaluate } from 'mathjs';
 const keys = document.querySelectorAll('.key');
 const display_input = document.querySelector('.display .input');
 const display_output = document.querySelector('.display .output');
